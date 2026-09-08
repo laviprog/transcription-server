f09574c57cb3b84694f989bccc8b325c0c00f8d3
laviprog <laviprog@ya.ru>
2026-09-08 15:58:07 +0300

    Rename POSTGRES_DB to POSTGRES_NAME in configuration files and update references

--- src/config.py
+++ src/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
 
     POSTGRES_HOST: str
     POSTGRES_PORT: int
-    POSTGRES_DB: str
+    POSTGRES_NAME: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
 
@@ -31,11 +31,11 @@ class Settings(BaseSettings):
 
     @property
     def DB_URL(self) -> str:
-        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_NAME}"
 
     @property
     def DB_URL_SYNC(self) -> str:
-        return f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        return f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_NAME}"
 
     @property
     def REDIS_URL(self) -> str:
